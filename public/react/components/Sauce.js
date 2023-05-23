f07d738aee415f9784c8c449ad6ed775cbb6a2ca
Natalia P <125387029+natalias-code@users.noreply.github.com>
2023-05-23 13:29:53 +0000

    abc

--- public/react/components/Sauce.js
+++ public/react/components/Sauce.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-export const Sauce = (props) => {
+export const Sauce = ({sauce}) => {
 
-  return <>
-    <h3>{props.sauce.name}</h3>
-    <img src={props.sauce.image} alt={props.sauce.name} />
+  return <> (
+  
+  {/*  <h3>{props.sauce.name}</h3>
+   <img src={props.sauce.image} alt={props.sauce.name} />*/}
+   <div className= "sauce-container">
+    <div className="img-container">
+      <img src={sauce.image} alt={sauce.name} className="sauce-image" />
+      <div className="sauce-info">
+        <h3 className="sauce-name">{sauce.name}</h3>
+        <p className="sauce-description">{sauce.description}</p>
+          <span className="sauce-category">{sauce.category}</span>
+          <span className="sauce-price">{sauce.price}</span>
+      </div>
+    </div>
+  </div>
+  );
   </>
-} 
+}
 	
