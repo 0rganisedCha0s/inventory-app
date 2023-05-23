790507645f234905c5e336f3550f3e3bf4cdc0be
ramon-almeida <ramonalmeidabkb@gmail.com>
2023-05-23 15:09:30 +0100

    Fetch request to add item when form is submitted

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { SaucesList } from './SaucesList';
+	import React, { useState, useEffect } from 'react';
+	import { SaucesList } from './SaucesList';
+	import  Form from './Form'
 
-// import and prepend the api url to any fetch calls
-import apiURL from '../api';
+	// import and prepend the api url to any fetch calls
+	import apiURL from '../api';
 
-export const App = () => {
+	export const App = () => {
 
-	const [sauces, setSauces] = useState([]);
+		const [sauces, setSauces] = useState([]);
 
-	async function fetchSauces(){
-		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
-			
-			setSauces(saucesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
+		async function fetchSauces(){
+			try {
+				const response = await fetch(`${apiURL}/sauces`);
+				const saucesData = await response.json();
+				
+				setSauces(saucesData);
+			} catch (err) {
+				console.log("Oh no an error! ", err)
+			}
 		}
+
+		useEffect(() => {
+			fetchSauces();
+		}, []);
+
+		return (
+			<main>	
+		<h1>Sauce Store</h1>
+				<h2>All things 🔥</h2>
+				<SaucesList sauces={sauces} />
+		<div>
+		<Form />
+		</div>
+			</main>
+		)
 	}
-
-	useEffect(() => {
-		fetchSauces();
-	}, []);
-
-	return (
-		<main>	
-      <h1>Sauce Store</h1>
-			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
-		</main>
-	)
-}
