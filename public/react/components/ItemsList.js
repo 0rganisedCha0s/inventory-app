9e90bbf3e1e49f8fa9e23283b88392b341a31280
Paul Preda <ppd1512@gmail.com>
2023-05-25 16:34:08 +0100

    fixed more bugs

--- public/react/components/ItemsList.js
+++ public/react/components/ItemsList.js
@@ -1,44 +1,16 @@
 import React from 'react';
 import { Item } from './Item';
-
-const ItemsList = ({ items, onItemClick }) => {
-  return (
- HEAD
-    <div>
- main
-	<div className="item-list">
-	{items.map((item, idx) => (
-	  <Item
-		item={item}
-		key={idx}
-		onItemClick={onItemClick}
-	  />
-	))}
-  </div>
-
-  );
-};
-
-export default ItemsList; 
-
-/*import React from 'react';
-import { Item } from './Item';
-
 const ItemsList = ({ items, onItemClick }) => {
   return (
     <div className="item-list">
-
- 37c1d39 (added SelectedItem, still need to style it further. did the styling for ItemList. User can now click on any item in list to display it and then return to the list of items)
-
-
-      {items.map((item) => (
-        <div key={item.id} onClick={() => onItemClick(item)}>
-          <Item item={item} />
-        </div>
-      ))}
-    </div>
+    {items.map((item, idx) => (
+      <Item
+        item={item}
+        key={idx}
+        onItemClick={onItemClick}
+      />
+    ))}
+  </div>
   );
 };
-
-
-
+export default ItemsList;
