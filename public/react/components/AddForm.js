80af106c7fe31107146e11d043ee1b4150aa8ad3
ramon-almeida <ramonalmeidabkb@gmail.com>
2023-05-26 15:10:27 +0100

    testing with jest AddForm.js

--- public/react/components/AddForm.js
+++ public/react/components/AddForm.js
@@ -1,106 +1,102 @@
-import React, { useState } from 'react';
+  import React, { useState } from 'react';
 
-const AddForm = () => {
-  const [formData, setFormData] = useState({});
-  const [submissionStatus, setSubmissionStatus] = useState('');
-  const [message, setMessage] = useState("");
-
-  const clearFields = (event) => {
-    setMessage(event.target.value);
-  };
+  const AddForm = () => {
+    const [formData, setFormData] = useState({});
+    const [submissionStatus, setSubmissionStatus] = useState('');
+  
 
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+      e.preventDefault();
 
-    const category = e.target.elements.category.value;
-    
-
-    const updatedFormData = {
-      ...formData,
-      category
+      const category = e.target.elements.category.value;
       
+
+      const updatedFormData = {
+        ...formData,
+        category
+        
+      };
+
+      try {
+        const response = await fetch('http://localhost:3000/api/items/add', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updatedFormData),
+        });
+
+        if (response.status === 200) {
+          setSubmissionStatus('Data submitted successfully!');
+          setFormData({});
+          window.location.reload(true)
+          e.target.reset(); // Reset the form fields
+          
+        } else {
+          setSubmissionStatus('Error submitting data: ' + response.status);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        setSubmissionStatus('Error: ' + error.message);
+      }
     };
 
-    try {
-      const response = await fetch('http://localhost:3000/api/items/add', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedFormData),
-      });
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
 
-      if (response.status === 200) {
-        setSubmissionStatus('Data submitted successfully!');
-        setFormData({});
-        window.location.reload(true)
-        e.target.reset(); // Reset the form fields
+  
+
+    return (
+      <form onSubmit={handleSubmit} className="form-container">
+        <h2>Add New Item</h2>
+
         
-      } else {
-        setSubmissionStatus('Error submitting data: ' + response.status);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      setSubmissionStatus('Error: ' + error.message);
-    }
+
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          required
+          onChange={handleChange} 
+          class="form-item"
+        />
+        <input
+          type="text"
+          name="price"
+          placeholder="Price"
+          required
+          
+          title="Enter a valid price (e.g., 10.99)"
+          onChange={handleChange}
+          class="form-item"
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          required
+          onChange={handleChange}
+          class="form-item"
+        />
+
+        <select name="category" onChange={handleChange} class="form-item">
+          <option value="men's clothing">Men's clothing</option>
+          <option value="jewelery">Jewelery</option>
+          <option value="electronics">Electronics</option>
+          <option value="women's clothing">Women's clothing</option>
+        </select>
+
+        <input type="text" name="image" placeholder="Image" onChange={handleChange}  class="form-item" />
+  
+        <button type="submit" class="btn-standard">Submit</button>
+  
+
+        {submissionStatus && <p>{submissionStatus}</p>}
+
+      </form>
+    );
   };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
- 
-
-  return (
-    <form onSubmit={handleSubmit} className="form-container">
-      <h2>Add New Item</h2>
-
-      <h3>Add Product</h3>
-
-      <input
-        type="text"
-        name="title"
-        placeholder="Title"
-        required
-        onChange={handleChange} 
-        class="form-item"
-      />
-      <input
-        type="text"
-        name="price"
-        placeholder="Price"
-        required
-        pattern="[0-9]+(\.[0-9]{1,2})?"
-        title="Enter a valid price (e.g., 10.99)"
-        onChange={handleChange}
-        class="form-item"
-      />
-      <input
-        type="text"
-        name="description"
-        placeholder="Description"
-        required
-        onChange={handleChange}
-        class="form-item"
-      />
-
-      <select name="category" onChange={handleChange} class="form-item">
-        <option value="men's clothing">Men's clothing</option>
-        <option value="jewelery">Jewelery</option>
-        <option value="electronics">Electronics</option>
-        <option value="women's clothing">Women's clothing</option>
-      </select>
-
-      <input type="text" name="image" placeholder="Image" onChange={handleChange}  class="form-item" />
- 
-      <button type="submit" class="btn-standard">Submit</button>
- 
-
-      {submissionStatus && <p>{submissionStatus}</p>}
-
-    </form>
-  );
-};
-
-export default AddForm;
+  export default AddForm;
